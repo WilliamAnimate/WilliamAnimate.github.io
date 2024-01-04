3dbf2ede84bb679a737cbc20dc8c71bee1a89536
William Animate <75321465+WilliamAnimate@users.noreply.github.com>
2024-01-04 11:51:35 -0500

    its not very responsive right now, but it works
    TODO: make responsive
    TODO: make it not bad
    TODO: stop writing TODOs

---
+++ js/script.js
@@ -0,0 +1,48 @@
+document.addEventListener("DOMContentLoaded", redirectAddEventlisteners);
+
+/**
+ * adds eventlisteners to the anchor element. when clicked, this will trigger the redirect animation.
+ * however, if the first hash of the window is "force_disable_redirect" then it will not redirect.
+ */
+function redirectAddEventlisteners() {
+	document.querySelector("a").addEventListener("click", function(e) {
+		e.preventDefault();
+
+		const href = this.getAttribute("href");
+		if (window.location.hash.substring(1) != "force_disable_redirect")
+			redirectLoadContent(href);
+		else
+			window.location = `${href}#force_disable_redirect`;
+	});
+}
+
+/**
+ * this will fade out the webpage and then fade it back in.
+ * @param {string} url the url to load content from
+ */
+function redirectLoadContent(url) {
+	document.body.style.opacity = 0;
+
+	let next;
+	fetch(url)
+		.then(response => response.text())
+		.then(html => next = html)
+		.catch(error => {
+			console.error("Can't get webpage, guess i'll force redirect now. ", error)
+			window.location = `${url}#force_disable_redirect`;
+		});
+
+	setTimeout(() => {
+		if (next) {
+			document.body.innerHTML = next;
+			document.body.style.opacity = 1; // TODO: css on document.body
+			redirectAddEventlisteners();
+		} else {
+			// TODO: implement edge case.
+			console.warn("not enough time was given to load the next navigation, redirecting manually.");
+			window.location = url;
+		}
+
+		window.history.pushState({}, "", url);
+	}, 200);
+}
